6560d0f40a65a58a0953c6ad41ecbbb86f812542
hyong <hyong_cs@outlook.com>
2021-08-11 22:21:13 +0800

    新增队列笔记

--- Stack/note.cpp
+++ Stack/note.cpp
@@ -8,7 +8,7 @@
  *  @Description :   栈 笔记
  */
 
-#define MaxSize 50
+#define MaxSize  50
 #define ElemType int
 
 typedef struct {
@@ -38,12 +38,58 @@ bool GetTop(SqStack S, ElemType &x) {
     return true;
 }
 
-typedef struct Linknode {
+typedef struct LinkNode {
     ElemType data;
-    struct Linknode *next;
+    struct LinkNode *next;
 } * LiStack;
 
 typedef struct {
     ElemType data[MaxSize];
     int front, rear;
 } SqQueue;
+
+void InitQueue(SqQueue &Q) { Q.rear = Q.front = 0; }
+
+bool isEmpty(SqQueue Q) { return Q.front == Q.rear; }
+
+bool EnQueue(SqQueue &Q, ElemType x) {
+    if ((Q.rear + 1) % MaxSize == Q.front) return false;
+    Q.data[Q.rear] = x;
+    Q.rear = (Q.rear + 1) % MaxSize;
+    return true;
+}
+
+bool DeQueue(SqQueue &Q, ElemType &x) {
+    if (isEmpty(Q)) return false;
+    x = Q.data[Q.front];
+    Q.front = (Q.front + 1) % MaxSize;
+    return true;
+}
+
+typedef struct {
+    LinkNode *front, *rear;
+} LinkQueue;
+
+void InitQueue(LinkQueue &Q) {
+    Q.front = Q.rear = new LinkNode();
+    Q.front->next = nullptr;
+}
+
+bool IsEmpty(LinkQueue Q) { return Q.front == Q.rear; }
+
+void EnQueue(LinkQueue &Q, ElemType x) {
+    LinkNode *s = new LinkNode();
+    s->data = x, s->next = nullptr;
+    Q.rear->next = s;
+    Q.rear = s;
+}
+
+bool DeQueue(LinkQueue &Q, ElemType &x) {
+    if (IsEmpty(Q)) return false;
+    LinkNode *p = Q.front->next;
+    x = p->data;
+    Q.front->next = p->next;
+    if (Q.rear == p) Q.rear = Q.front;
+    delete p;
+    return true;
+}
